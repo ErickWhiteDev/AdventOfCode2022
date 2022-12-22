65eb9952fba3ad5156babefb0cf75202f5f40cc9
Erick White <erickwhitebusiness@gmail.com>
2022-12-21 17:45:20 -0700

    Finished day 8

--- cpp/8/1/main.cpp
+++ cpp/8/1/main.cpp
@@ -16,10 +16,10 @@ int main() {
 
     int width = 0;
     int height = 0;
+    int count = 0;
+    int max;
 
     string inputLine;
-
-    vector<int> compare;
 
     while(getline(input, inputLine)) {
         if (!width) {
@@ -33,35 +33,86 @@ int main() {
 
     input.open("input.txt");
 
-    int treeArray[height + 2][width + 2];
-    int visArray[height + 2][width + 2];
-    int checkArray[height + 2][width + 2];
+    int treeArray[height][width];
+    int visArray[height][width]; // Mark which trees are visible
 
-    memset(treeArray, 0, sizeof(treeArray));
+    memset(treeArray, 0, sizeof(treeArray)); // Fill arrays with zeros
     memset(visArray, 0, sizeof(visArray));
-    memset(checkArray, 0, sizeof(checkArray));
 
-    for (int i = 1; i <= height; i++) {
+    for (int i = 0; i < height; i++) { // Populate array of trees from input
         getline(input, inputLine);
-        for (int j = 1; j <= width; j++) {
-            treeArray[i][j] = inputLine[j - 1] - '0';
+
+        for (int j = 0; j < width; j++) {
+            treeArray[i][j] = inputLine[j] - '0';
         }
     }
 
     input.close();
 
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= width; j++) {
-            compare.clear();
-            compare.push_back(treeArray[i][j]);
-            compare.push_back(treeArray[i - 1][j]);
-            compare.push_back(treeArray[i + 1][j]);
-            compare.push_back(treeArray[i][j - 1]);
-            compare.push_back(treeArray[i][j + 1]);
-            sort(compare.begin(), compare.end());
-            
+    for (int i = 0; i < height; i++) { // Scan horizontally for visible trees
+        max = -1;
+
+        for (int j = 0; j < width; j++) { // Scan from left side
+            if (treeArray[i][j] > max) {
+                visArray[i][j] = 1;
+                max = treeArray[i][j];
+
+                if (treeArray[i][j] == 9) {
+                    break;
+                }
+            }
+        }
+
+        max = -1;
+
+        for (int j = height - 1; j >= 0; j--) { // Scan from right side
+            if (treeArray[i][j] > max) {
+                visArray[i][j] = 1;
+
+                max = treeArray[i][j];
+
+                if (treeArray[i][j] == 9) {
+                    break;
+                }
+            }
         }
     }
+
+    for (int i = 0; i < width; i++) { // Scan vertically for visible trees
+        max = -1;
+
+        for (int j = 0; j < height; j++) { // Scan from top
+            if(treeArray[j][i] > max) {
+                visArray[j][i] = 1;
+                max =  treeArray[j][i];
+
+                if (treeArray[j][i] == 9) {
+                    break;
+                }
+            }
+        }
+
+        max = -1;
+
+        for (int j = height - 1; j >= 0; j--) { // Scan from bottom
+            if (treeArray[j][i] > max) {
+                visArray[j][i] = 1;
+                max = treeArray[j][i];
+
+                if (treeArray[j][i] == 9) {
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < height; i++) { // Sum up visible trees from visibility array
+        for (int j = 0; j < width; j++) {
+            count += visArray[i][j];
+        }
+    }
+
+    cout << count << endl;
 
     return 0;
 }
